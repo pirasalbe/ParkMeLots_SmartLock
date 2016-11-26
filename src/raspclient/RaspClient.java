c301e3ca6a1a4e6026483d492002f084dfbe8b2f
giovanni stefania <giovanni stefania>
2016-11-26 17:22:13 +0100

    update from file

--- src/raspclient/RaspClient.java
+++ src/raspclient/RaspClient.java
@@ -8,7 +8,8 @@ import java.util.StringTokenizer;
 class RaspClient
 {
     public static Socket connection;
-    public static String tosend, receive, servername = "10.100.1.89", key = "0", signcode = "0", datastart = "01/01/2000-10:00", dataend = "02/02/2001-11:11", position = "0,0-0,0";
+    public static String tosend, receive, servername = "10.100.1.89", key = "0", signcode = "0", position = "0,0-0,0";
+    public static long  datastart, dataend; 
     public static int updatecounter = 1, port = 2000, timer = 5000;
     public static byte[] res, length = new byte[4];
     public static InputStream in;
@@ -17,6 +18,7 @@ class RaspClient
     public static PrintWriter out;
     public static DataInputStream dIn;
     public static DataOutputStream dOut;
+    public static String Fname = "clientconf.txt";
     
     
     public static void main(String[] args)
@@ -30,13 +32,14 @@ class RaspClient
         String line = "";
         try
         {
-            File file = new File("clientconf.txt");
+            File file = new File(Fname);
             if (!file.exists())
             {
                 file.createNewFile();
-                writeonfile("0");
+                writeonfile("0"); //set default value
             }
             
+            //read the file
             FileReader reader = new FileReader(file);
             BufferedReader buffReader = new BufferedReader(reader);
             String s;
@@ -50,11 +53,12 @@ class RaspClient
             System.err.println(e);
         }
         
-        String[] art1 = tokened(line);
+        String[] art1 = tokened(line); //splits string
         
         return art1;
     }
     
+    //divides string
     public static String[] tokened(String tosep)
     {
         String tmp = tosep;
@@ -70,6 +74,7 @@ class RaspClient
         return out;
     }
     
+    //send my values
     public static void update()
     {
         tosend = "UPT";
@@ -95,13 +100,9 @@ class RaspClient
         {
             String content = rkey + ";" + signcode + ";" + datastart + ";" + dataend + ";" + position;
             
-            File file = new File("clientconf.txt");
-
-            if (!file.exists())
-            {
-                    file.createNewFile();
-            }
+            File file = new File(Fname);
             
+            //write on file 
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(content);
@@ -154,31 +155,42 @@ class RaspClient
         return strRes;
     }
     
-    public static void initialize() 
+    public static void updateValues()
     {
+        //initialize file
         String[] cat = initfilereading();
         
-        assign(cat);
+        assign(cat); //set values from my file
+    }
+    
+    public static void initialize() 
+    {
+        updateValues();
         
         try
         {
+            //socket
             connection = new Socket(servername, port);
             System.out.println("Connected to the server.");
+            
+            //initialize server
             tosend = "SGN";
             send();
             
+            //key generator
             tosend = key;
             send();
             
             if (key.equals("0"))
             {
-                key = recv();
+                key = recv(); //receive from server
             }
             System.out.println("Received new key from the server.");
             //first();
             while(true)
             {
-                update();
+                updateValues();
+                update(); //send update
             }
         }
         catch (UnknownHostException e)
@@ -195,8 +207,8 @@ class RaspClient
     {
         key = input[0];
         signcode = input[1];
-        datastart = input[2];
-        dataend = input[3];
+        datastart = Long.parseLong(input[2]);
+        dataend = Long.parseLong(input[3]);
         position = input[4];
     }
 }
